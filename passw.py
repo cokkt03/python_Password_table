db8cd62916116d669db453579ed9236150a9862d
cokkt03 <56531406+cokkt03@users.noreply.github.com>
2021-05-31 11:25:04 +0800

    Add files via upload

--- passw.py
+++ passw.py
@@ -1,24 +1,5 @@
-""" import random
-random.seed()  #设置随机种子数
-    #设置种子选择空间
-s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-ls = [] #存取密码的列表
-FirstPsw = "" #存取第一个密码的字符
- 
-while len(ls)<100:  #十个随机密码
-    pwd = ""
-
-    for i in range(8):
-        pwd += s[random.randint(0,len(s)-1)]
-    if pwd[0] in FirstPsw:
-        continue
-    else:
-        ls.append(pwd)
-        FirstPsw +=pwd[0]
-    print(pwd)
-fo = open("password.txt","w",encoding ="utf-8")
-fo.write("\n".join(ls))
-fo.close() """
+# !/usr/bin/python
+# coding:utf-8
 import time
 start = time.process_time()
 
